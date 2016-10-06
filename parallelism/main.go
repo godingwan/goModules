82c889bba967b9dbb93d4f5e7fc0730c43b04b7a
Joshua <jsungholee@gmail.com>
2016-10-06 15:35:52 -0400

    parallelism comments

--- parallelism/main.go
+++ parallelism/main.go
@@ -6,26 +6,29 @@ import (
 	"time"
 )
 
+/* sync package allows synchronization */
 var wg sync.WaitGroup
 
 func main() {
-	wg.Add(2)
-	go foo()
-	go bar()
+	wg.Add(2) // when the number reaches 0 the goroutines on wait are released
+	go first()
+	go other()
 	wg.Wait()
 }
 
-func foo() {
+func first() {
 	for i := 0; i < 45; i++ {
 		fmt.Println("Foo: ", i)
+		// waits 3 millisecond before calling the next interation
 		time.Sleep(time.Duration(3 * time.Millisecond))
 	}
 	wg.Done()
 }
 
-func bar() {
+func other() {
 	for i := 0; i < 45; i++ {
 		fmt.Println("Bar: ", i)
+		// waits 20 millisecond before calling the next iteration
 		time.Sleep(time.Duration(20 * time.Millisecond))
 	}
 	wg.Done()
